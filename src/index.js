5b9b10597f35a15b289c2ab7ecacd1ed85013117
HariKrishna-28 <harikrishnac2002@gmail.com>
2022-05-04 10:01:31 +0530

    refactor

--- src/index.js
+++ src/index.js
@@ -3,22 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 // eslint-disable-next-line
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
-import { createBrowserHistory } from "history";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const history = createBrowserHistory();
 
 root.render(
   <React.StrictMode>
-    <HashRouter history={history}>
-      {/* <BrowserRouter> */}
+    <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
-      {/* </BrowserRouter> */}
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
